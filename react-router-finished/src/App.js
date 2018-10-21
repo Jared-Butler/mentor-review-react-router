7bf5fdd53f1e9968bdbcc6fa168fdcc159399c76
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-10-21 17:58:33 -0600

    Basic functionality works

--- react-router-finished/src/App.js
+++ react-router-finished/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import routes from "./routes"
+import {Link} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        {routes}
       </div>
     );
   }
