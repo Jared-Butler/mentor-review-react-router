b664e86543b1d745eb316d84d414473439cef1ba
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-10-21 18:04:30 -0600

    Added components to the starter file

---
+++ react-router-starter/src/components/About.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class About extends Component{
+    render(){
+        return (
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis lectus nulla at volutpat diam. Eleifend quam adipiscing vitae proin sagittis nisl. Sed velit dignissim sodales ut eu sem integer vitae. Lacus luctus accumsan tortor posuere ac ut. Turpis nunc eget lorem dolor sed viverra ipsum nunc. </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis lectus nulla at volutpat diam. Eleifend quam adipiscing vitae proin sagittis nisl. Sed velit dignissim sodales ut eu sem integer vitae. Lacus luctus accumsan tortor posuere ac ut. Turpis nunc eget lorem dolor sed viverra ipsum nunc. </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis lectus nulla at volutpat diam. Eleifend quam adipiscing vitae proin sagittis nisl. Sed velit dignissim sodales ut eu sem integer vitae. Lacus luctus accumsan tortor posuere ac ut. Turpis nunc eget lorem dolor sed viverra ipsum nunc. </p>
+            </div>
+        )
+    }
+}
+
+export default About
---
+++ react-router-starter/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Home extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Home</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis lectus nulla at volutpat diam. Eleifend quam adipiscing vitae proin sagittis nisl. Sed velit dignissim sodales ut eu sem integer vitae. Lacus luctus accumsan tortor posuere ac ut. Turpis nunc eget lorem dolor sed viverra ipsum nunc. </p>
+            </div>
+        )
+    }
+}
+
+export default Home
